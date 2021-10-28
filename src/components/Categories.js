4154360bb71f89e7c8a6ce29c78c761a1ad60b45
Vanessa Maciel <veesoria@gmail.com>
2021-10-28 04:12:41 -0700

    fine tuning layout

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,9 +3,13 @@ import Category from "./Category";
 const Categories = () => {
   return (
     <div className="categories">
-      <Category image={"./images/womens.jpg"} title="Women's" />
-      <Category image={"./images/mens.jpg"} title="Men's" />
-      <Category image={"./images/accessories.jpg"} title="Accessories" mobileImage="./images/accessoriesMobile.jpg" hasMobileImage="true" />
+      <div className="mobileRow">
+        <Category image={"./images/womens.jpg"} title="Women's" />
+        <Category image={"./images/mens.jpg"} title="Men's" />
+      </div>
+      <div className="mobileRow">
+        <Category image={"./images/accessories.jpg"} title="Accessories" mobileImage="./images/accessoriesMobile.jpg" hasMobileImage="true" />
+      </div>
     </div>
   );
 };
--- src/components/HeroCta.js
+++ src/components/HeroCta.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
+
 const HeroCta = ({image, title, text, link, textColor}) => {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    let mediaQuery = window.matchMedia("(min-width: 830px)");
+    setMobile(!mediaQuery.matches);
+
+    const updateMobile = (e) => {
+      setMobile(!e.matches);
+    };
+
+    mediaQuery.addEventListener("change", updateMobile);
+
+    return () => mediaQuery.removeEventListener("change", updateMobile);
+  }, []);
+
+  const color = mobile ? "#10504f" : textColor;
+
   return (
     <div className="heroCta">
       <img src={image} alt="hero" />
       <div className="cta">
-        <h1 className="green">{title}</h1>
-        <p className="green">{text}</p>
+        <h1 style={{color: color}}>{title}</h1>
+        <p style={{color: color}}>{text}</p>
         <button>Shop Now</button>
       </div>
     </div>
